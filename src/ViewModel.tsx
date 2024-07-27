d95655982dd2c0ae2c86ca03cd3859c44ae3bb86
AmirGhiassian <amirrezaghiassian@icloud.com>
2024-07-27 04:35:39 -0400

    added mat

--- src/ViewModel.tsx
+++ src/ViewModel.tsx
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { useEffect, useRef } from "react";
+import { STLLoader } from "three/examples/jsm/Addons.js";
 
 function ViewModel() {
   const refContainer = useRef<HTMLDivElement>(null);
@@ -12,14 +13,24 @@ function ViewModel() {
       0.1,
       1000,
     );
+    const material = new THREE.MeshPhysicalMaterial({
+      color: 0xb2ffc8,
+      metalness: 0.25,
+      roughness: 0.1,
+      opacity: 1.0,
+      transparent: true,
+      transmission: 0.99,
+      clearcoat: 1.0,
+      clearcoatRoughness: 0.25,
+    });
     var renderer = new THREE.WebGLRenderer();
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     // document.body.appendChild( renderer.domElement );
     // use ref as a mount point of the Three.js scene instead of the document.body
     refContainer.current &&
       refContainer.current.appendChild(renderer.domElement);
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     camera.position.z = 5;
